8d57c2b4a417c0d32c8244dbfa728a021f0874ae
mckelveygreg <mckelveygreg@gmail.com>
2018-09-19 21:57:48 -0700

    touch replaced, but still not working...

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-
-
-// var projectTilesLink = document.querySelectorAll('#project-grid a');
-
-// projectTilesLink.forEach(tile => {
-//   //  touchEventAdd(tile);
-// });
-
-// var projectTilesText = document.querySelectorAll('#project-grid a p');
-
-// projectTilesText.forEach(tile => {
-//     touchEventAdd(tile);    
-// });
-
 var mobileText = document.querySelector('#mobileText');
 function clearScreen() {
     mobileText.style.display = 'none';
@@ -19,63 +5,76 @@ function clearScreen() {
 }
 mobileText.addEventListener('click', clearScreen, false);
 
-// // Solution found at https://gist.github.com/SleepWalker/da5636b1abcbaff48c4d
-// // It was the final rendition of this
+var projectTilesLink = document.querySelectorAll('#project-grid a');
 
-// let pageWidth = window.innerWidth || document.body.clientWidth;
-// let treshold = Math.max(1,Math.floor(0.01 * (pageWidth)));
-// let touchstartX = 0;
-// let touchstartY = 0;
-// let touchendX = 0;
-// let touchendY = 0;
+projectTilesLink.forEach(tile => {
+  //  touchEventAdd(tile);
+});
 
-// const limit = Math.tan(45 * 1.5 / 180 * Math.PI);
-// //const gestureZone = document.getElementById('mobileText');
+var projectTilesText = document.querySelectorAll('#project-grid a p');
 
-// // I put the gestureZones in a function that is the callback for the event listeners
-// function touchEventAdd (gestureZone) {
-//     gestureZone.addEventListener('touchstart', function(event) {
-//         touchstartX = event.changedTouches[0].screenX;
-//         touchstartY = event.changedTouches[0].screenY;
-//     }, false);
+projectTilesText.forEach(tile => {
+    touchEventAdd(tile);    
+});
+
+
+// Solution found at https://gist.github.com/SleepWalker/da5636b1abcbaff48c4d
+// It was the final rendition of this
+
+let pageWidth = window.innerWidth || document.body.clientWidth;
+let treshold = Math.max(1,Math.floor(0.01 * (pageWidth)));
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+const limit = Math.tan(45 * 1.5 / 180 * Math.PI);
+//const gestureZone = document.getElementById('mobileText');
+
+// I put the gestureZones in a function that is the callback for the event listeners
+function touchEventAdd (gestureZone) {
+    gestureZone.addEventListener('touchstart', function(event) {
+        touchstartX = event.changedTouches[0].screenX;
+        touchstartY = event.changedTouches[0].screenY;
+    }, false);
     
-//     gestureZone.addEventListener('touchend', function(event) {
-//         touchendX = event.changedTouches[0].screenX;
-//         touchendY = event.changedTouches[0].screenY;
-//         handleGesture(event);
-//     }, false);
+    gestureZone.addEventListener('touchend', function(event) {
+        touchendX = event.changedTouches[0].screenX;
+        touchendY = event.changedTouches[0].screenY;
+        handleGesture(event);
+    }, false);
     
-// }
+}
 
-// function handleGesture(e) {
-//     let box = e.target;
-//     let x = touchendX - touchstartX;
-//     let y = touchendY - touchstartY;
-//     let xy = Math.abs(x / y);
-//     let yx = Math.abs(y / x);
-//     if (Math.abs(x) > treshold || Math.abs(y) > treshold) {
-//         if (yx <= limit) {
-//             if (x < 0) {
-//                 console.log("left");
-//             } else {
-//                 console.log("right");
-//             }
-//             //I only care if it is left or right.
-//             // Swipe classes are added to let hover effects show
-//             if (box.getAttribute('class') == 'swiped') {
-//                 box.removeAttribute('class', 'swiped');
-//             } else {
-//                 box.setAttribute('class', 'swiped');
-//             } 
-//         }
-//         if (xy <= limit) {
-//             if (y < 0) {
-//                 console.log("top");
-//             } else {
-//                 console.log("bottom");
-//             }
-//         }
-//     } else {
-//         console.log("tap");
-//     }
-// }
+function handleGesture(e) {
+    let box = e.target;
+    let x = touchendX - touchstartX;
+    let y = touchendY - touchstartY;
+    let xy = Math.abs(x / y);
+    let yx = Math.abs(y / x);
+    if (Math.abs(x) > treshold || Math.abs(y) > treshold) {
+        if (yx <= limit) {
+            if (x < 0) {
+                console.log("left");
+            } else {
+                console.log("right");
+            }
+            //I only care if it is left or right.
+            // Swipe classes are added to let hover effects show
+            if (box.getAttribute('class') == 'swiped') {
+                box.removeAttribute('class', 'swiped');
+            } else {
+                box.setAttribute('class', 'swiped');
+            } 
+        }
+        if (xy <= limit) {
+            if (y < 0) {
+                console.log("top");
+            } else {
+                console.log("bottom");
+            }
+        }
+    } else {
+        console.log("tap");
+    }
+}
