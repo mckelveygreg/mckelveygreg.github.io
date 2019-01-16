3576a2315e3f869e4ade79c2a26d30cb4aedbb4e
Greg McKelvey <mckelveygreg@gmail.com>
2019-01-16 13:26:52 -0800

    npm audit fix, content updates

---
+++ precache-manifest.05374667985fdb1ecc5c3aeb5ed810ae.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "58254db353ca5d0b48c4",
+    "url": "/static/css/main.59ab63ec.chunk.css"
+  },
+  {
+    "revision": "58254db353ca5d0b48c4",
+    "url": "/static/js/main.58254db3.chunk.js"
+  },
+  {
+    "revision": "e4d48e4555c49385c13e",
+    "url": "/static/js/1.e4d48e45.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "37d1ff6ae65adcd2c9e2b638b20b8cf1",
+    "url": "/static/media/tree_background.37d1ff6a.png"
+  },
+  {
+    "revision": "98ad5e5e14cb8add687b896114bcfcbf",
+    "url": "/static/media/lunch-survey.98ad5e5e.jpg"
+  },
+  {
+    "revision": "9efb52b2cb3cc314dd3aa54c47b97e9c",
+    "url": "/static/media/product-landing.9efb52b2.jpg"
+  },
+  {
+    "revision": "175b6b79e293857e6a502e32a35e2399",
+    "url": "/static/media/tech-documentation.175b6b79.jpg"
+  },
+  {
+    "revision": "1d317289c8e6022fb602f6d0e0bab336",
+    "url": "/static/media/js-calculator.1d317289.jpg"
+  },
+  {
+    "revision": "b34c4e48464a1b22d426670b32e79c0e",
+    "url": "/static/media/Simon-game.b34c4e48.jpg"
+  },
+  {
+    "revision": "04b64e24640d340f391fa78787aa0fd7",
+    "url": "/static/media/dad_jokes.04b64e24.png"
+  },
+  {
+    "revision": "149a6e38b3e82eb5d9187f0b0ffd38f3",
+    "url": "/static/media/markdown_previewer.149a6e38.png"
+  },
+  {
+    "revision": "415a755005733d29942fc57864e2bf20",
+    "url": "/static/media/drum-machine.415a7550.jpg"
+  },
+  {
+    "revision": "5a1bdd1a6351e9e77597013283621fd6",
+    "url": "/static/media/d3-bar-graph.5a1bdd1a.jpg"
+  },
+  {
+    "revision": "170c0e1cfcf1d0f6123983079e43c462",
+    "url": "/static/media/scatterplot.170c0e1c.jpg"
+  },
+  {
+    "revision": "0a407979030a5660c0530c4625d668ce",
+    "url": "/static/media/d3_heat_map.0a407979.png"
+  },
+  {
+    "revision": "d32d501440c0792af03cf333572ab92e",
+    "url": "/static/media/d3_choropleth_map.d32d5014.png"
+  },
+  {
+    "revision": "2810823b8cee3163d0254888e5e7bb00",
+    "url": "/static/media/d3_treemap.2810823b.png"
+  },
+  {
+    "revision": "a3c81d5a1c346caba206fea2a95b1b86",
+    "url": "/static/media/100-days-of-code.a3c81d5a.jpg"
+  },
+  {
+    "revision": "d9ba4dac17fd6a4297631df39010d50c",
+    "url": "/static/media/legacy_front_end.d9ba4dac.png"
+  },
+  {
+    "revision": "3713071e90fee6a847bc64ee1e930bb2",
+    "url": "/static/media/responsive_web_design_cert.3713071e.png"
+  },
+  {
+    "revision": "395f1742d4c047620d222dae6d3c5dd3",
+    "url": "/static/media/JavaScript_Data_Structures_Cert.395f1742.png"
+  },
+  {
+    "revision": "6496d81f4c7398223e05abedb1a3bb38",
+    "url": "/static/media/Frontend_libraries.6496d81f.png"
+  },
+  {
+    "revision": "59a31f6e35ad8855ffdfe850a251fce7",
+    "url": "/static/media/Data_Visualization.59a31f6e.jpg"
+  },
+  {
+    "revision": "aa0e218f03c1a1db0c8bc5bf410e85fb",
+    "url": "/static/media/APIs-and-Microservices.aa0e218f.jpg"
+  },
+  {
+    "revision": "5255cc84ce502a994a89bcdbc5355964",
+    "url": "/index.html"
+  }
+];
